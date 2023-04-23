0b8641d084e4f61a0013eb746ea41e186cdf9d03
A1 exe <tspears2@hawk.iit.edu>
2023-04-23 01:35:23 -0500

    Testing

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -7,25 +7,19 @@
 use blog_os::println;
 use core::panic::PanicInfo;
 
-#[no_mangle]
+#[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
-    #[cfg(test)]
     test_main();
 
     loop {}
 }
 
-#[cfg(not(test))]
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    println!("{}", _info);
-    loop {}
+fn panic(info: &PanicInfo) -> ! {
+    blog_os::test_panic_handler(info)
 }
 
-#[cfg(test)]
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    blog_os::test_panic_handler(_info)
+#[test_case]
+fn test_println() {
+    println!("test_println output");
 }
