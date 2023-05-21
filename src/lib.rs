2da75c4066653c35c7271dfd7cf30098ae3fa6d3
A1 exe <tspears2@hawk.iit.edu>
2023-05-20 23:15:59 -0500

    Double Faults

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@
 #![reexport_test_harness_main = "test_main"]
 #![feature(abi_x86_interrupt)]
 
+pub mod gdt;
 pub mod interrupts;
 pub mod serial;
 pub mod vga_buffer;
@@ -43,6 +44,7 @@ pub fn test_panic_handler(info: &PanicInfo) -> ! {
 
 // Global general init procedure
 pub fn init() {
+    gdt::init();
     interrupts::init_idt();
 }
 
