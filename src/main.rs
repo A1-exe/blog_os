3ade6a7fd6003bc2798c5364011f25324efab004
A1 exe <tspears2@hawk.iit.edu>
2023-05-27 18:06:03 -0500

    Traverse L4 and L3 page table example

--- src/main.rs
+++ src/main.rs
@@ -5,28 +5,45 @@
 #![reexport_test_harness_main = "test_main"]
 
 use blog_os::println;
+use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
+use x86_64::{structures::paging::PageTable, PhysAddr, VirtAddr};
 
+entry_point!(kernel_main);
+
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
+    use blog_os::memory::active_level_4_table;
+
+    println!("Hello World{}", "!");
     blog_os::init();
 
-    let ptr = 0x204ef6 as *mut u64;
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let l4_table = unsafe { active_level_4_table(phys_mem_offset) };
 
-    unsafe {
-        let x = *ptr;
-    };
-    println!("read success");
+    for (i, entry) in l4_table.iter().enumerate() {
+        if !entry.is_unused() {
+            println!("L4 Entry {}: {:?}", i, entry);
 
-    unsafe { *ptr = 69 };
-    println!("write success");
+            // get and convert physical address
+            let phys = entry.frame().unwrap().start_address();
+            let virt = boot_info.physical_memory_offset + phys.as_u64();
+            let ptr = VirtAddr::new(virt).as_mut_ptr();
+            let l3_table: &PageTable = unsafe { &*ptr };
+
+            // print non-emptry l3 entries
+            for (i, entry) in l3_table.iter().enumerate() {
+                if !entry.is_unused() {
+                    println!("    L3 Entry {}: {:?}", i, entry);
+                }
+            }
+        }
+    }
 
     #[cfg(test)]
     test_main();
 
-    println!("It did not crash!");
+    println!("It didn't crash!");
     blog_os::hlt_loop();
 }
 
